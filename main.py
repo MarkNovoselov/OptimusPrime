c11f8f45e0d331b06820b28ac8a3bed025f32a82
MarkNovoselov <167557089+MarkNovoselov@users.noreply.github.com>
2026-03-20 14:16:20 +0200

    Refactor bot to use FastAPI with webhook support

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import telebot
+from fastapi import FastAPI, Request
 
-bot = telebot.TeleBot('8792672663:AAHv4ORGXc_wlSAPg6XqStqRUNsDvMYPmZI')
+TOKEN = '8792672663:AAHv4ORGXc_wlSAPg6XqStqRUNsDvMYPmZI'
+WEBHOOK_URL = "https://optimusprime-48zy.onrender.com/webhook"
+
+bot = telebot.TeleBot(TOKEN)
+app = FastAPI()
+
 
 @bot.message_handler(commands=['start'])
-def start_messsage(message):
-    bot.send_message(message.chat.id, 'пашел нахуй')
+def start_message(message):
+    bot.send_message(message.chat.id, "Hi from webhook 👋")
 
-bot.infinity_polling(none_stop=True,interval=0)
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    update = telebot.types.Update.de_json(data)
+    bot.process_new_updates([update])
+    return {"ok": True}
+
+
+@app.on_event("startup")
+async def on_startup():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL)
